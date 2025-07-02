eab7344d8444b2bd7f17742832b785e871cb012b
James Geater <jgeater@outlook.com>
2025-07-02 08:24:27 -0400

    Add project files.

---
+++ PE_Machine_Disabled/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace PE_Machine_Disabled
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(347, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(347, 341);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Shutdown";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(511, 78);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "THIS MACHINE HAS BEEN DISABLED. \r\n\r\nThis maching as been disabled because it does" +
+    " not comply with Allstate Security policies\r\nIt will shutdown in 60 seconds\r\n\r\nC" +
+    "lick OK to shutdown now\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1019, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PE_Machine_Disabled/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace PE_Machine_Disabled
+{
+    public partial class Form1 : Form
+    {
+        private List<Form> blackScreens = new List<Form>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string logfile = @"C:\PKGLOG\PE_Machine_Disabled.log";
+            // cmdline option setsched is specified, create the scheduled task
+            if (Environment.GetCommandLineArgs().Contains("setsched"))
+            {
+                //copy self to c:\ebicode\PE_Machine_Disabled.exe
+                string sourcePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                string destinationPath = @"C:\ebicode\PE_Machine_Disabled.exe";
+                try
+                {
+                    System.IO.File.Copy(sourcePath, destinationPath, true);
+                }
+                catch (Exception ex)
+                {
+                    // Handle the exception (e.g., log it, show a message, etc.)
+                    System.Windows.MessageBox.Show("Error copying file: " + ex.Message);
+                    Environment.Exit(0);
+                }
+
+                // Create the scheduled task here
+                // 2 tasks one shuts the machine down 60 minutes after boot, the other one runs C:\ebicode\PE_Machine_Disabled.exe at user logon
+                string taskName = "PE_Machine_Disabled";
+                // Create a task to run at user logon
+                try
+                {
+                    using (StreamWriter w = File.AppendText(logfile))
+                    {
+                        Log("Creating Scheduled task", w);
+                    }
+                    using (var resource = Assembly.GetExecutingAssembly().GetManifestResourceStream("PE_Machine_Disabled.PE_Machine_Disabled_Logon.xml"))
+                    {
+                        using (var file = new FileStream("c:\\ebicode\\PE_Machine_Disabled_Logon.xml", FileMode.Create, FileAccess.Write))
+                        {
+                            resource.CopyTo(file);
+                        }
+                    }
+                    //now import the xml file to create the scheduled task    
+                    try
+                    {
+                        var setsched = new ProcessStartInfo();
+                        setsched.FileName = "schtasks.exe";
+                        setsched.UseShellExecute = false;
+                        setsched.Arguments = @"/Create /XML c:\ebicode\PE_Machine_Disabled_Logon.xml /tn PE_Machine_Disabled_Logon /F";
+                        setsched.CreateNoWindow = true;
+                        var p1 = Process.Start(setsched);
+                        p1.WaitForExit();
+
+                        File.Delete("c:\\ebicode\\PE_Machine_Disabled_Logon.xml");
+                        using (StreamWriter w = File.AppendText(logfile))
+                        {
+                            Log("Done Setting schedule, exiting now", w);
+                        }
+                        //Environment.Exit(0);
+                    }
+                    catch (Exception ex)
+                    {
+                        using (StreamWriter w = File.AppendText(logfile))
+                        {
+                            Log("Error creating schedule.", w);
+                            Log(ex.Message, w);
+                        }
+                    }
+                    using (StreamWriter w = File.AppendText(logfile))
+                    {
+                        Log("Scheduled task created successfully", w);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    using (StreamWriter w = File.AppendText(logfile))
+                    {
+                        Log(ex.Message, w);
+                    }
+                    System.Windows.MessageBox.Show(ex.Message);
+                }
+
+                //now do the shutdown task using PE_Machine_Disabled_Shutdown.xml
+                try
+                {
+                    using (StreamWriter w = File.AppendText(logfile))
+                    {
+                        Log("Creating Scheduled task for shutdown", w);
+                    }
+                    using (var resource = Assembly.GetExecutingAssembly().GetManifestResourceStream("PE_Machine_Disabled.PE_Machine_Disabled_Shutdown.xml"))
+                    {
+                        using (var file = new FileStream("c:\\ebicode\\PE_Machine_Disabled_Shutdown.xml", FileMode.Create, FileAccess.Write))
+                        {
+                            resource.CopyTo(file);
+                        }
+                    }
+                    //now import the xml file to create the scheduled task    
+                    try
+                    {
+                        var setsched = new ProcessStartInfo();
+                        setsched.FileName = "schtasks.exe";
+                        setsched.UseShellExecute = false;
+                        setsched.Arguments = @"/Create /XML c:\ebicode\PE_Machine_Disabled_Shutdown.xml /tn PE_Machine_Disabled_Shutdown /F";
+                        setsched.CreateNoWindow = true;
+                        var p1 = Process.Start(setsched);
+                        p1.WaitForExit();
+                        File.Delete("c:\\ebicode\\PE_Machine_Disabled_Shutdown.xml");
+                        using (StreamWriter w = File.AppendText(logfile))
+                        {
+                            Log("Done Setting schedule, exiting now", w);
+                        }
+                        //Environment.Exit(0);
+                    }
+                    catch (Exception ex)
+                    {
+                        using (StreamWriter w = File.AppendText(logfile))
+                        {
+                            Log("Error creating schedule.", w);
+                            Log(ex.Message, w);
+                        }
+                    }
+                    using (StreamWriter w = File.AppendText(logfile))
+                    {
+                        Log("Scheduled task for shutdown created successfully", w);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    using (StreamWriter w = File.AppendText(logfile))
+                    {
+                        Log(ex.Message, w);
+                    }
+                    System.Windows.MessageBox.Show(ex.Message);
+                }
+
+                Environment.Exit(0);
+
+            }
+
+
+            // cmdline option delsched is specified, delete the scheduled task
+            if (Environment.GetCommandLineArgs().Contains("delsched"))
+            {
+                // Delete the scheduled task here
+                string taskName = "PE_Machine_Disabled";
+                System.Diagnostics.Process.Start("schtasks", $"/delete /tn \"{taskName}_Logon\" /f");
+                System.Diagnostics.Process.Start("schtasks", $"/delete /tn \"{taskName}_Shutdown\" /f");
+
+                //System.Windows.MessageBox.Show("Scheduled task deleted successfully.");
+                Environment.Exit(0);
+            }
+
+            // look for PE_Machine_Disabled.txt in the current directory
+            if (System.IO.File.Exists("PE_Machine_Disabled.txt"))
+            {
+                // read the file and display the contents in label1
+                string message = System.IO.File.ReadAllText("PE_Machine_Disabled.txt");
+                label1.Text = message;
+            }
+            else
+            {
+                // if the file does not exist, display a default message
+                
+            }
+
+
+            //scale the UI elements based on screen resolution
+            float fontSize = Math.Min(this.ClientSize.Width, this.ClientSize.Height) / 30f; // Adjust the divisor to change the scaling factor
+            label1.Font = new Font(label1.Font.FontFamily, fontSize, FontStyle.Bold);
+
+            //scale button1 and button2 based on screen resolution
+            button1.Font = new Font(button1.Font.FontFamily, fontSize, FontStyle.Bold);
+            button2.Font = new Font(button2.Font.FontFamily, fontSize, FontStyle.Bold);
+            //scale the buttons to fit the font size
+            button1.Width = (int)(this.ClientSize.Width * 0.3f); // 30% of the form width
+            button2.Width = (int)(this.ClientSize.Width * 0.3f); // 30% of the form width
+            button1.Height = (int)(fontSize * 2.5f); // Adjust height based on font size
+            button2.Height = (int)(fontSize * 2.5f); // Adjust height based on font size
+
+
+            button1.Visible = false;
+            // Check to see if user is local admin
+            // If user is local admin, show button1
+            if (IsUserLocalAdmin())
+            {
+                button1.Visible = true;
+            }
+          
+            //show form1 full screen anmd topmost on main screen
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            this.TopMost = true;
+            this.Location = new Point(0, 0);
+            this.StartPosition = FormStartPosition.Manual;
+            this.ShowInTaskbar = false;
+
+            //center justify lable1, button 1 and button 2  
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            button1.TextAlign = ContentAlignment.MiddleCenter;
+            button2.TextAlign = ContentAlignment.MiddleCenter;
+            //place lable1 center screen
+            label1.Location = new Point((this.ClientSize.Width - label1.Width) / 2, (this.ClientSize.Height - label1.Height) / 2);
+            //place button1 below label1
+            button1.Location = new Point((this.ClientSize.Width - button1.Width) / 2, label1.Bottom + 10);
+            //place button2 below button1   
+            button2.Location = new Point((this.ClientSize.Width - button2.Width) / 2, button1.Bottom + 10);
+
+            // Set dispaly black screen on all secondary monitors
+            foreach (Screen s in Screen.AllScreens)
+            {
+                if (s != Screen.PrimaryScreen)
+                {
+                    Form blackScreen = new Form();
+                    blackScreen.FormBorderStyle = FormBorderStyle.None;
+                    blackScreen.StartPosition = FormStartPosition.Manual;
+                    blackScreen.Bounds = s.Bounds;
+                    blackScreen.BackColor = Color.Black;
+                    blackScreen.ShowInTaskbar = false;
+                    blackScreen.TopMost = true;
+                    blackScreen.Show();
+
+                    blackScreens.Add(blackScreen); // Keep a reference
+                }
+            }
+        }
+
+
+        private bool IsUserLocalAdmin()
+        {
+            // Check if the current user is a local administrator
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //shutdown the machine
+            System.Diagnostics.Process.Start("shutdown", "/s /t 0");
+            Environment.Exit(0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Environment.Exit(0);
+        }
+
+        public static void Log(string logMessage, TextWriter w)
+        {
+            w.WriteLine("{0}: {1}: {2}", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString(), logMessage);
+        }
+    }
+}
